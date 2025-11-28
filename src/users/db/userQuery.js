c27d26f01b8efd47cd8845e8c3bc37e8e0591511
kdh <znfnfns0365c@gmail.com>
2025-11-28 17:00:34 +0900

    ✨ update :  userQuery.js 수정

--- src/users/db/userQuery.js
+++ src/users/db/userQuery.js
@@ -1,5 +1,5 @@
 export const SQL_QUERIES = {
     FIND_USERID_BY_KAKAOID: 'SELECT * FROM users WHERE kakao_id = ?',
-    INSERT_USER: 'INSERT INTO Users (user_id, kakao_id) VALUES (?, ?)',
+    INSERT_USER: 'INSERT INTO users (user_id, kakao_id) VALUES (?, ?)',
     FIND_KAKAOID_BY_USERID: 'SELECT kakao_id FROM users WHERE user_id = ?',
 };
