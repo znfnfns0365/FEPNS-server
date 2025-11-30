957c6acba8da12cc1808846d784789448b96c22c
kdh <znfnfns0365c@gmail.com>
2025-12-01 00:42:31 +0900

    🩹 fix:  단어 변경

--- src/db/users/userDb.js
+++ src/db/users/userDb.js
@@ -1,22 +1,22 @@
 import pools from '../database.js';
-import { SQL_QUERIES } from './userQuery.js';
+import { USER_QUERIES } from './userQuery.js';
 
 export const findUserIdByKakaoId = async (kakaoId) => {
-    const [rows] = await pools.fepns.query(SQL_QUERIES.FIND_USERID_BY_KAKAOID, [kakaoId]);
+    const [rows] = await pools.fepns.query(USER_QUERIES.FIND_USERID_BY_KAKAOID, [kakaoId]);
     return rows[0];
 };
 
 export const insertUserIdByKakaoId = async (userId, kakaoId) => {
-    const [result] = await pools.fepns.query(SQL_QUERIES.INSERT_USER, [userId, kakaoId]);
+    const [result] = await pools.fepns.query(USER_QUERIES.INSERT_USER, [userId, kakaoId]);
     return result;
 };
 
 export const findKakaoIdByUserId = async (userId) => {
-    const [rows] = await pools.fepns.query(SQL_QUERIES.FIND_KAKAOID_BY_USERID, [userId]);
+    const [rows] = await pools.fepns.query(USER_QUERIES.FIND_KAKAOID_BY_USERID, [userId]);
     return rows[0];
 };
 
 export const findUserByUserId = async (userId) => {
-    const [rows] = await pools.fepns.query(SQL_QUERIES.FIND_USER_BY_USERID, [userId]);
+    const [rows] = await pools.fepns.query(USER_QUERIES.FIND_USER_BY_USERID, [userId]);
     return rows[0];
 };
--- src/db/users/userQuery.js
+++ src/db/users/userQuery.js
@@ -1,4 +1,4 @@
-export const SQL_QUERIES = {
+export const USER_QUERIES = {
     FIND_USERID_BY_KAKAOID: 'SELECT * FROM users WHERE kakao_id = ?',
     INSERT_USER: 'INSERT INTO users (user_id, kakao_id) VALUES (?, ?)',
     FIND_KAKAOID_BY_USERID: 'SELECT kakao_id FROM users WHERE user_id = ?',
