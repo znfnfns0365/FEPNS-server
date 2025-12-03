ca01c84fe2fc05b530c37ebc45ed1877b035eadc
kdh <znfnfns0365c@gmail.com>
2025-12-03 11:01:22 +0900

    🔥 del: 안 쓰는 jwt token 삭제

--- src/constant/env.js
+++ src/constant/env.js
@@ -5,8 +5,6 @@ dotenv.config();
 export const PORT = process.env.PORT;
 export const HOST = process.env.HOST;
 
-export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-
 // DB 환경 변수
 export const DB_HOST = process.env.DB_HOST;
 export const DB_PORT = process.env.DB_PORT || 3306;
